5ac268c3158a515cc08ac17e77dde608584f92a8
Junseok Lee <boris0730@gmail.com>
2021-09-13 17:22:30 +0900

    명언 생성기

---
+++ IOS_QuotesGenerator/IOS_QuotesGenerator/Quote.swift
@@ -0,0 +1,13 @@
+//
+//  Quote.swift
+//  IOS_QuotesGenerator
+//
+//  Created by Junseok Lee on 2021/09/13.
+//
+
+import Foundation
+
+struct Quote {
+    let contents: String
+    let name: String
+}
